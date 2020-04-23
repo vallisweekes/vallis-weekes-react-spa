f47515d582c156945fbfa0e099bc3a31a783a167
vallisweekes <vallis.weekes@gmail.com>
2020-04-23 15:23:45 +0100

    exprience added

--- src/components/container/About/Expirience.jsx
+++ src/components/container/About/Expirience.jsx
@@ -12,7 +12,18 @@ const Expirience = () => {
         <AboutMePageLeft>
           <h2>Experience</h2>
         </AboutMePageLeft>
-        <AboutMePageRight></AboutMePageRight>
+        <AboutMePageRight>
+          <h2>Vision Direct</h2>
+          <p>Web Delivery Executive</p>
+          <p>
+            Operate in fast-paced, e-commerce, Agile environment to migrate
+            content across the company’s 7 markets (Magento to coreMedia). Lead
+            staff onboarding procedures and translate technical requirements to
+            non-technical personnel. Collaborate with project owner and
+            non-technical team, attend brainstorming sessions, and conduct
+            research; design UX prior to launch.
+          </p>
+        </AboutMePageRight>
       </AboutPageInner>
     </AboutMeExpirience>
   );
